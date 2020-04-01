9e3207b49a1cefcf9de6c5fa186e7365683f59ea
Andrei Klimenok <jaguarislove@gmail.com>
2020-04-01 18:18:27 +0200

    add arguments, option

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import program from 'commander';
 
 const startUtil = () => {
-  program.version('0.0.1');
+  program
+    .version('0.0.1')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      console.log(firstConfig, secondConfig);
+    })
+    .option('-f, --format <format>', 'output format');
   program.description('Compares two configuration files and shows a difference.');
   program.parse(process.argv);
 };
